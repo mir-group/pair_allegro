01a21157a1f4c6b25a5bcfb940bfa56ce9e5a4bd
Anders Johansson <anjohan@uio.no>
2021-08-09 13:28:39 -0400

    first commit

---
+++ pair_dice.cpp
@@ -0,0 +1,332 @@
+/* ----------------------------------------------------------------------
+   LAMMPS - Large-scale Atomic/Molecular Massively Parallel Simulator
+   https://lammps.sandia.gov/, Sandia National Laboratories
+   Steve Plimpton, sjplimp@sandia.gov
+
+   Copyright (2003) Sandia Corporation.  Under the terms of Contract
+   DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
+   certain rights in this software.  This software is distributed under
+   the GNU General Public License.
+
+   See the README file in the top-level LAMMPS directory.
+------------------------------------------------------------------------- */
+
+/* ----------------------------------------------------------------------
+   Contributing author: Anders Johansson (Harvard)
+------------------------------------------------------------------------- */
+
+#include <pair_dice.h>
+#include "atom.h"
+#include "comm.h"
+#include "domain.h"
+#include "error.h"
+#include "force.h"
+#include "memory.h"
+#include "neigh_list.h"
+#include "neigh_request.h"
+#include "neighbor.h"
+#include "potential_file_reader.h"
+#include "tokenizer.h"
+
+#include <cmath>
+#include <cstring>
+#include <numeric>
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <torch/script.h>
+#include <c10/cuda/CUDACachingAllocator.h>
+
+
+using namespace LAMMPS_NS;
+
+PairDICE::PairDICE(LAMMPS *lmp) : Pair(lmp) {
+  restartinfo = 0;
+  manybody_flag = 1;
+
+  if(torch::cuda::is_available()){
+    device = torch::kCUDA;
+  }
+  else {
+    device = torch::kCPU;
+  }
+  std::cout << "DICE is using device " << device << "\n";
+}
+
+PairDICE::~PairDICE(){
+  if (allocated) {
+    memory->destroy(setflag);
+    memory->destroy(cutsq);
+  }
+}
+
+void PairDICE::init_style(){
+  if (atom->tag_enable == 0)
+    error->all(FLERR,"Pair style DICE requires atom IDs");
+
+  // need a full neighbor list
+  int irequest = neighbor->request(this,instance_me);
+  neighbor->requests[irequest]->half = 0;
+  neighbor->requests[irequest]->full = 1;
+
+  neighbor->requests[irequest]->ghost = 1;
+
+  if (force->newton_pair == 0)
+    error->all(FLERR,"Pair style DICE requires newton pair on");
+}
+
+double PairDICE::init_one(int i, int j)
+{
+  return cutoff;
+}
+
+void PairDICE::allocate()
+{
+  allocated = 1;
+  int n = atom->ntypes;
+
+  memory->create(setflag,n+1,n+1,"pair:setflag");
+  memory->create(cutsq,n+1,n+1,"pair:cutsq");
+}
+
+void PairDICE::settings(int narg, char ** /*arg*/) {
+  // "flare" should be the only word after "pair_style" in the input file.
+  if (narg > 0)
+    error->all(FLERR, "Illegal pair_style command, too many arguments");
+}
+
+void PairDICE::coeff(int narg, char **arg) {
+  if (!allocated)
+    allocate();
+
+  // Should be exactly 3 arguments following "pair_coeff" in the input file.
+  if (narg != 3)
+    error->all(FLERR, "Incorrect args for pair coefficients");
+
+  // Ensure I,J args are "* *".
+  if (strcmp(arg[0], "*") != 0 || strcmp(arg[1], "*") != 0)
+    error->all(FLERR, "Incorrect args for pair coefficients");
+
+  int n = atom->ntypes;
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+      setflag[i][j] = 0;
+
+  // set setflag i,j for type pairs where both are mapped to elements
+
+  int count = 0;
+  for (int i = 1; i <= n; i++)
+    for (int j = i; j <= n; j++)
+        setflag[i][j] = 1;
+
+  std::cout << "Loading model from " << arg[2] << "\n";
+
+  std::unordered_map<std::string, std::string> metadata = {
+    {"config", ""},
+    {"nequip_version", ""},
+    {"r_max", ""},
+    {"n_species", ""}
+  };
+  model = torch::jit::load(std::string(arg[2]), device, metadata);
+
+  std::cout << "Information from model: " << metadata.size() << " key-value pairs\n";
+  for( const auto& n : metadata ) {
+    std::cout << "Key:[" << n.first << "] Value:[" << n.second << "]\n";
+  }
+
+  cutoff = std::stod(metadata["r_max"]);
+
+  // TODO: Make remaining arguments species-mapping
+  // See SW.
+}
+
+// Force and energy computation
+void PairDICE::compute(int eflag, int vflag){
+  ev_init(eflag, vflag);
+
+  // Get info from lammps:
+
+  // Atom positions, including ghost atoms
+  double **x = atom->x;
+  // Atom forces
+  double **f = atom->f;
+  // Atom IDs, unique, reproducible, the "real" indices
+  // Probably 1-based
+  tagint *tag = atom->tag;
+  // Atom types, 1-based
+  int *type = atom->type;
+  // Number of local/real atoms
+  int nlocal = atom->nlocal;
+  // Whether Newton is on (i.e. reverse "communication" of forces on ghost atoms).
+  // Should probably be off.
+  int newton_pair = force->newton_pair;
+
+  // Number of local/real atoms
+  int inum = list->inum;
+  assert(inum==nlocal); // This should be true, if my understanding is correct
+  // Number of ghost atoms
+  int nghost = list->gnum;
+  // Total number of atoms
+  int ntotal = inum + nghost;
+  // Mapping from neigh list ordering to x/f ordering
+  int *ilist = list->ilist;
+  // Number of neighbors per atom
+  int *numneigh = list->numneigh;
+  // Neighbor list per atom
+  int **firstneigh = list->firstneigh;
+
+  // Total number of bonds (sum of number of neighbors)
+  int nedges = std::accumulate(numneigh, numneigh+ntotal, 0);
+
+  torch::Tensor pos_tensor = torch::zeros({nlocal, 3});
+  torch::Tensor edges_tensor = torch::zeros({2,nedges}, torch::TensorOptions().dtype(torch::kInt64));
+  torch::Tensor edge_cell_shifts_tensor = torch::zeros({nedges,3});
+  torch::Tensor tag2type_tensor = torch::zeros({nlocal}, torch::TensorOptions().dtype(torch::kInt64));
+  torch::Tensor periodic_shift_tensor = torch::zeros({3});
+  torch::Tensor cell_tensor = torch::zeros({3,3});
+
+  auto pos = pos_tensor.accessor<float, 2>();
+  auto edges = edges_tensor.accessor<long, 2>();
+  auto edge_cell_shifts = edge_cell_shifts_tensor.accessor<float, 2>();
+  auto tag2type = tag2type_tensor.accessor<long, 1>();
+  auto periodic_shift = periodic_shift_tensor.accessor<float, 1>();
+  auto cell = cell_tensor.accessor<float,2>();
+
+  // Inverse mapping from tag to "real" atom index
+  std::vector<int> tag2i(inum);
+
+  // Loop over real atoms to store tags, types and positions
+  for(int ii = 0; ii < inum; ii++){
+    int i = ilist[ii];
+    int itag = tag[i];
+    int itype = type[i];
+
+    // Inverse mapping from tag to x/f atom index
+    tag2i[itag-1] = i; // tag is probably 1-based
+    tag2type[itag-1] = itype-1;
+    pos[itag-1][0] = x[i][0];
+    pos[itag-1][1] = x[i][1];
+    pos[itag-1][2] = x[i][2];
+  }
+
+  // Get cell
+  cell[0][0] = domain->boxhi[0] - domain->boxlo[0];
+
+  cell[1][0] = domain->xy;
+  cell[1][1] = domain->boxhi[1] - domain->boxlo[1];
+
+  cell[2][0] = domain->xz;
+  cell[2][1] = domain->yz;
+  cell[2][2] = domain->boxhi[2] - domain->boxlo[2];
+
+  /*
+  std::cout << "cell: " << cell_tensor << "\n";
+  std::cout << "tag2i: " << "\n";
+  for(int itag = 0; itag < inum; itag++){
+    std::cout << tag2i[itag] << " ";
+  }
+  std::cout << std::endl;
+  */
+
+  auto cell_inv = cell_tensor.inverse().transpose(0,1);
+
+  // Loop over atoms and neighbors,
+  // store edges and _cell_shifts
+  // ii follows the order of the neighbor lists,
+  // i follows the order of x, f, etc.
+  int edge_counter = 0;
+  for(int ii = 0; ii < ntotal; ii++){
+    int i = ilist[ii];
+    int itag = tag[i];
+    int itype = type[i];
+
+    int jnum = numneigh[i];
+    int *jlist = firstneigh[i];
+    for(int jj = 0; jj < jnum; jj++){
+      int j = jlist[jj];
+      j &= NEIGHMASK;
+      int jtag = tag[j];
+      int jtype = type[j];
+
+      // TODO: check sign
+      periodic_shift[0] = x[j][0] - pos[jtag-1][0];
+      periodic_shift[1] = x[j][1] - pos[jtag-1][1];
+      periodic_shift[2] = x[j][2] - pos[jtag-1][2];
+
+      double dx = x[i][0] - x[j][0];
+      double dy = x[i][1] - x[j][1];
+      double dz = x[i][2] - x[j][2];
+
+      double rsq = dx*dx + dy*dy + dz*dz;
+      assert(rsq < cutoff*cutoff);
+
+      torch::Tensor cell_shift_tensor = cell_inv.matmul(periodic_shift_tensor);
+      auto cell_shift = cell_shift_tensor.accessor<float, 1>();
+      edge_cell_shifts[edge_counter][0] = std::round(cell_shift[0]);
+      edge_cell_shifts[edge_counter][1] = std::round(cell_shift[1]);
+      edge_cell_shifts[edge_counter][2] = std::round(cell_shift[2]);
+      //std::cout << "cell shift: " << cell_shift_tensor << "\n";
+
+      // TODO: double check order
+      edges[0][edge_counter] = itag - 1; // tag is probably 1-based
+      edges[1][edge_counter] = jtag - 1; // tag is probably 1-based
+
+      edge_counter++;
+    }
+  }
+
+  //std::cout << "tag2type: " << tag2type_tensor << "\n";
+  //std::cout << "Edges: " << edges_tensor << "\n";
+  //std::cout << "Edge _cell_shifts: " << edge_cell_shifts_tensor << "\n";
+
+  c10::Dict<std::string, torch::Tensor> input;
+  input.insert("pos", pos_tensor.to(device));
+  input.insert("edge_index", edges_tensor.to(device));
+  input.insert("edge_cell_shift", edge_cell_shifts_tensor.to(device));
+  input.insert("cell", cell_tensor.to(device));
+  input.insert("species_index", tag2type_tensor.to(device));
+  std::vector<torch::IValue> input_vector(1, input);
+
+  auto output = model.forward(input_vector).toGenericDict();
+
+  torch::Tensor forces_tensor = output.at("forces").toTensor().cpu();
+  auto forces = forces_tensor.accessor<float, 2>();
+
+  torch::Tensor total_energy_tensor = output.at("total_energy").toTensor().cpu();
+
+  // store the total energy where LAMMPS wants it
+  eng_vdwl = total_energy_tensor.data_ptr<float>()[0];
+
+  torch::Tensor atomic_energy_tensor = output.at("atomic_energy").toTensor().cpu();
+  auto atomic_energies = atomic_energy_tensor.accessor<float, 2>();
+  float atomic_energy_sum = atomic_energy_tensor.sum().data_ptr<float>()[0];
+
+  //std::cout << "atomic energy sum: " << atomic_energy_sum << std::endl;
+  //std::cout << "Total energy: " << total_energy_tensor << "\n";
+  //std::cout << "atomic energy shape: " << atomic_energy_tensor.sizes()[0] << "," << atomic_energy_tensor.sizes()[1] << std::endl;
+  //std::cout << "atomic energies: " << atomic_energy_tensor << std::endl;
+
+  // Write forces and per-atom energies (0-based tags here)
+  for(int itag = 0; itag < inum; itag++){
+    int i = tag2i[itag];
+    f[i][0] = forces[itag][0];
+    f[i][1] = forces[itag][1];
+    f[i][2] = forces[itag][2];
+    if (eflag_atom) eatom[i] = atomic_energies[itag][0];
+    //printf("%d %d %g %g %g %g %g %g\n", i, type[i], pos[itag][0], pos[itag][1], pos[itag][2], f[i][0], f[i][1], f[i][2]);
+  }
+
+  // TODO: Virial stuff? (If there even is a pairwise force concept here)
+
+  // TODO: Performance: Depending on how the graph network works, using tags for edges may lead to shitty memory access patterns and performance.
+  // It may be better to first create tag2i as a separate loop, then set edges[edge_counter][:] = (i, tag2i[jtag]).
+  // Then use forces(i,0) instead of forces(itag,0).
+  // Or just sort the edges somehow.
+
+  /*
+  if(device.is_cuda()){
+    //torch::cuda::empty_cache();
+    c10::cuda::CUDACachingAllocator::emptyCache();
+  }
+  */
+}
