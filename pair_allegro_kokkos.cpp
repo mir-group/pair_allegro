554f4c01311c25905ac620d36a9302b768988cc1
Hongyu-yu <hongyuyu20@fudan.edu.cn>
2022-12-16 13:47:38 +0800

    kokkos bug fix

--- pair_allegro_kokkos.cpp
+++ pair_allegro_kokkos.cpp
@@ -118,7 +118,6 @@ void PairAllegroKokkos<DeviceType>::compute(int eflag_in, int vflag_in)
 
   x = atomKK->k_x.view<DeviceType>();
   f = atomKK->k_f.view<DeviceType>();
-  virial = atomKK->k_virial.view<DeviceType>();
   tag = atomKK->k_tag.view<DeviceType>();
   type = atomKK->k_type.view<DeviceType>();
   nlocal = atom->nlocal;
